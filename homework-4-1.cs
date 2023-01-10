cb507077ae0ce610cc952af2cda91fa4085590dd
VakurinSergej <120665026+VakurinSergej@users.noreply.github.com>
2023-01-10 18:55:28 +0300

    Add files via upload
    M homework in task 4

---
+++ homework-4-1.cs
@@ -0,0 +1,14 @@
+﻿// Цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Без использования Math.Pow
+
+Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine());
+int result = 1;
+int n = 1;
+while (n <= b)
+{
+    result = result * a;
+    n++;
+}
+Console.WriteLine(result);
---
+++ homework-4-2.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая принимает на вход число и выдаёт сумму цифр в числе
+Console.WriteLine("Введите число: ");
+string str = Console.ReadLine();
+int sum = 0;
+
+for (int i = 0; i < str.Length; i++)
+{
+    char currentChar = str[i]; // '1'
+    // для примера пытаемся получить число сразу из символа
+    int positionOfChar = Convert.ToInt32(currentChar); // 49 - порядковый номер '1' в таблице ascii
+    // символ currentChar приводим к строке (меняем тип данных на string)
+    string stringChar = currentChar.ToString(); // "1" можно убрать эту строку из кода
+    int numberFromChar = int.Parse(stringChar); // число 1
+    // Console.Write($"Сивол: {currentChar}, позиция: {positionOfChar}, число: {numberFromChar} \n");
+}
+
+for (int i = 0; i < str.Length; i++)
+{
+    int currentNumber = int.Parse(str[i].ToString());
+    sum += currentNumber; // sum = sum + currentNumber
+}
+Console.WriteLine(sum);
